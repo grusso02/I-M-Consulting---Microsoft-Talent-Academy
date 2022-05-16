c44ddaa1135a0a91b34105b1ff000c64b6828093
grusso02 <russogabriele45@mail.com>
2022-05-16 21:03:43 +0200

    start a project that print citizen from region, province and town

---
+++ FirstHtml/script/js/myLogic.js
@@ -0,0 +1,16 @@
+/* $(document).ready(function() {
+    alert("Page is loaded, document ready");
+    console.log( "ready!" );
+}); */
+
+function ViewRegione() {
+    //alert("Page is loaded");
+    var divReg = document.getElementById("divRegione");
+    //kdivReg.hidden = "false";
+
+    var divPro = document.getElementById("divProvincia")
+    divPro.hidden = "true";
+
+    var divCom = document.getElementById("divComune");
+    divCom.hidden = "true";
+}
