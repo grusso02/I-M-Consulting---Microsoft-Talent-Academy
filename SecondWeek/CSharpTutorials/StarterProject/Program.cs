a1dd7f2078e936ed590f85d87691ad0acbbf5b8d
grusso02 <russogabriele45@mail.com>
2022-05-23 17:28:14 +0200

    Some concept of C#

---
+++ SecondWeek/CSharpTutorials/StarterProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StarterProject.Helper;
+
+namespace StarterProject
+{
+    class Program
+    {
+        /// <summary>
+        /// Il metodo main contiene l'entry point dell'applicazione e viene chiamato a Runtime come metodo statico
+        /// </summary>
+        /// <param name="args">Array di stringhe passate in input dalla riga di comando</param>
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle(4);
+            Vehicle vehicle2 = vehicle;
+            Console.WriteLine(object.ReferenceEquals(vehicle, vehicle2));
+
+            Console.WriteLine("Inserisci il primo numero"); // Si Inserisce nello stream output
+            string num1 = Console.ReadLine(); // Si Inserisce nello stream input
+
+            Console.WriteLine("Inserisci il secondo numero");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine(Int32.Parse(num1) + Int32.Parse(num2));
+
+            ConsoleKeyInfo key = Console.ReadKey(); // Prende in input un tasto premuto sulla keyboard
+            Console.WriteLine(key.KeyChar); // Stamp il valore (char) del tasto premuto
+        }
+    }
+}
---
+++ SecondWeek/CSharpTutorials/StarterProject/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterProject.Helper
+{
+    /// <summary>
+    /// Possiamo dichiarare una classe internal (utilizzabile solo all'interno del progetto) default
+    /// o public (utilizzabile ancha da progetti esterni)
+    /// </summary>
+    internal class Vehicle
+    {
+        #region Constructor
+        public Vehicle(int _wheels) : this(_wheels, 0, 0, false) {}
+        public Vehicle(int _wheels, int _speed, int _direction, bool _engineRunning)
+        {
+            wheels = _wheels;
+            speed = _speed;
+            direction = _direction;
+            engineRunning = _engineRunning;
+        }
+        #endregion
+
+        //region rende il codice interno collassabile
+        #region Public fields
+        public int wheels;
+        public float speed;
+        public int direction;
+        #endregion
+
+        #region Private fields
+        private bool engineRunning;
+        #endregion
+    }
+}
