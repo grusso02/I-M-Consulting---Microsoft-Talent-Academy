6cfe294497b38b605d24c25df867e332188abdf5
grusso02 <russogabriele45@mail.com>
2022-05-26 09:20:30 +0200

    SearchFlights

---
+++ SecondWeek/SearchFlights/SearchFlights/Flights.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    class Flights
+    {
+        private string _carrier;
+        private string _originCyteName;
+        private string _destCyteName;
+        private int _depDelay;
+        private int _arrDelay;
+        private bool _cancelled;
+        private double _distance;
+
+        public Flights() {}
+        public Flights(string[] splitted)
+        {
+            _carrier = splitted[0];
+            _originCyteName = splitted[1];
+            _destCyteName = splitted[2];
+            if (!Int32.TryParse(splitted[3], out _depDelay))
+                _depDelay = 0;
+            if (!Int32.TryParse(splitted[4], out _arrDelay))
+                _depDelay = 0;
+            _cancelled = Convert.ToBoolean(Convert.ToInt32(splitted[5]));
+            _distance = Convert.ToDouble(splitted[6]);
+
+        }
+
+        public string Carrier { get; }
+        public string OriginCyteName { get; }
+        public string DestCyteName { get; }
+        public int DepDelay { get; }
+        public int ArrDelay { get; }
+        public bool Cancelled { get; }
+        public double Distance { get; }
+    }
+}
---
+++ SecondWeek/SearchFlights/SearchFlights/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Infrastructure;
+
+namespace SearchFlights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Flights> flights = new List<Flights>();
+
+            Console.WriteLine("Please insert Origin");
+            string origin = Console.ReadLine();
+            Console.WriteLine("Please insert Destination");
+            string destination = Console.ReadLine();
+
+            ReadFile(flights, origin, destination);
+            PrintFlights(flights);
+        }
+        public static void ReadFile(List<Flights> flights, string origin, string destination)
+        {
+            string filepath = @"C:\Microsoft-Talent-Academy\SecondWeek\SearchFlights\SearchFlights\flights.csv";
+
+            if (File.Exists(filepath))
+            {
+                using (StreamReader sr = File.OpenText(filepath))
+                {
+                    string s;
+                    sr.ReadLine(); //skip the first line
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        string[] splitted = s.Split(",");
+                        if (origin == splitted[1] && destination == splitted[2])
+                            flights.Add(new Flights(splitted));
+                    }
+                }
+            }
+        }
+        public static void PrintFlights(List<Flights> flights)
+        {
+            foreach (Flights flight in flights)
+                Console.Write("Carrier: {0}\t Dep_Delay: {1}\t Arr_Delay: {2}\t Cancelles: {3}\t Distance: {4}\n",
+                    flight.Carrier, flight.DepDelay, flight.ArrDelay, flight.Cancelled, flight.Distance);
+        }
+    }
+}
