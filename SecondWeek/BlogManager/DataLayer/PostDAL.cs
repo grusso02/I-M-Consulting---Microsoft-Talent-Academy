7eb923713fd25a2b542837b90cce76cea55384cd
grusso02 <russogabriele45@mail.com>
2022-05-27 17:03:28 +0200

    http request (get) in a windows form

---
+++ SecondWeek/BlogManager/DataLayer/PostDAL.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class PostDAL
+    {
+        private readonly string _url = "https://jsonplaceholder.typicode.com/posts";
+
+        public async Task<IEnumerable<Post>> GetPostsAsync()
+        {
+            IEnumerable<Post> posts = null;
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                var response = await client.GetAsync(_url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    posts = JsonSerializer.Deserialize<IEnumerable<Post>>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return posts;
+        }
+    }
+}
